f4c5bb5f5e4ac7c8a4157d6798a08509ee987f5d
sunset wan <sunsetwan@gmail.com>
2022-05-13 00:19:34 +0800

    fix: remove Main.storyboard

--- HeartRateMonitor/AppDelegate.swift
+++ HeartRateMonitor/AppDelegate.swift
@@ -36,6 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    return true
+      let window = UIWindow()
+      window.rootViewController = ReceiveiPadBatteryUpdateViewController()
+      window.makeKeyAndVisible()
+      self.window = window
+
+      return true
   }
 }
